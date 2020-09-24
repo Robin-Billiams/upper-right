2660ca48ad4db1606d14bd550f0d82508c44ce90
David Boyea <davidmcruz1125@gmail.com>
2020-09-24 18:52:45 -0500

    Builds a schema with propper syntax

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -3,5 +3,17 @@ mongoose.connect( 'mongodb://localhost/test', { useNewUrlParser: true } );
 const db = mongoose.connection;
 db.on( 'error', console.error.bind( console, 'connection error:' ) );
 db.once( 'open', () => {
+  const statSchema = new mongoose.Schema({
+    name: String,
+    modelNum: Number,
+    averageReview: {
+      stars: Number,
+      votes: Number,
+    },
+    insPlan2Yr: Number, //2 year insurance plan
+    insPlan4Yr: Number, //4 year insurance plan
+    installments: Number, //over 48 months 0% APR
+    buyNow: Number
+  });
   console.log('We are connected!!');
 });
