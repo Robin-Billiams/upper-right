4c7d6adb0e43c4d7f20533d705c728c6b2c6088b
David Boyea <davidmcruz1125@gmail.com>
2020-09-24 19:32:53 -0500

    Refactors test Scehma and uses a method to add data to database

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,19 +1,23 @@
 const mongoose = require( 'mongoose' );
-mongoose.connect( 'mongodb://localhost/test', { useNewUrlParser: true } );
+mongoose.connect( 'mongodb://localhost/test' );
 const db = mongoose.connection;
+const Kitten = require( './buyOption.js' );
 db.on( 'error', console.error.bind( console, 'connection error:' ) );
-db.once( 'open', () => {
-  const statSchema = new mongoose.Schema({
-    name: String,
-    modelNum: Number,
-    averageReview: {
-      stars: Number,
-      votes: Number,
-    },
-    insPlan2Yr: Number, //2 year insurance plan
-    insPlan4Yr: Number, //4 year insurance plan
-    installments: Number, //over 48 months 0% APR
-    buyNow: Number
-  });
-  console.log('We are connected!!');
-});
+
+  // const optionsSchema = new mongoose.Schema({
+  //   name: String,
+  //   modelNum: Number,
+  //   averageReview: {
+  //     stars: Number,
+  //     votes: Number,
+  //   },
+  //   insPlan2Yr: Number, //2 year insurance plan
+  //   insPlan4Yr: Number, //4 year insurance plan
+  //   installments: Number, //over 48 months 0% APR
+  //   buyNow: Number
+  // });
+
+  // const Options = mongoose.model( 'Options', optionsSchema );
+  const silence = new Kitten( { name: 'Silence' } );
+  db.collection('kitties').insertOne(silence);
+  console.log( silence.name );
