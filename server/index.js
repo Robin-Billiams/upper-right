b459ae144e8aa126ad4d7c1a0fe07d1248b33d69
David Boyea <davidmcruz1125@gmail.com>
2020-09-25 22:38:53 -0500

    Creates a working API without request listeners

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
-// app.get('/api/customizeAndPurchase', ( req, res ) {
+const db = require( './index.js' );
+const express = require('express');
+const app = express();
 
-// });
+app.use(express.json());
+app.use(express.urlencoded);
+
+const port = 3001;
+
+
+
+
+
+app.listen(port, () => {
+  console.log( `listening on port ${port}` );
+});
