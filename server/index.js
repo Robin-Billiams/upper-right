b7c7b7c9534d3a4e81cf52bde65ecaf90ea1cb5e
David Boyea <davidmcruz1125@gmail.com>
2020-09-25 23:04:06 -0500

    app.get handler returns all of the database information

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
+const Options = require( '../mongoDB/buyOption.js' );
 const db = require( '../mongoDB/index.js' );
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.json());
+// app.use(express.urlencoded());
 
 const port = 3001;
 
 app.get('/api/options', ( req, res ) => {
   // res.send('okay');
-  db.collection( 'options' ).findAsync()
-  .then((result) => { console.log(req.body)})
-  .then(res.send('ok'));
+  Options.find().exec()// queries automaticaly return promises .exec() allows use of this
+  .then( ( result ) => { res.send( result ) } )
 });
 
 
