cc5f0cafa13f979e60a864c3d9af2f611a4e819b
David Boyea <davidmcruz1125@gmail.com>
2020-09-25 22:48:40 -0500

    API works to send an 'okay' to postman

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
-const db = require( './index.js' );
+const db = require( '../mongoDB/index.js' );
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded);
+// app.use(express.json());
+// app.use(express.urlencoded);
 
 const port = 3001;
+
+app.get('/api/options', ( req, res ) => {
+  res.send('okay');
+  // db.collection( 'options' ).find()
+  // .then((result) => { console.log(result)})
+  // .then(res.send('ok'));
+});
 
 
 
