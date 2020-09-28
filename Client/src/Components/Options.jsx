bfcfd855e7136751bb1ee5b94a4c89e3eeb0b678
David Boyea <davidmcruz1125@gmail.com>
2020-09-28 17:22:06 -0500

    Rough structure to index.jsx

---
+++ Client/src/Components/Options.jsx
@@ -0,0 +1,25 @@
+const react = require('react');
+
+class Options extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    },
+
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+// $.ajax({
+//   url: `/api/customizePurchase`,
+//   type: `GET`,
+//   data: JSON.stringify( selection ),
+//   success: ( result ) => {
+//     this.setState( { \affected\: result})
+//   }
+// })
--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -2,4 +2,5 @@ const react = require('react');
 const reactDOM = require('react-dom');
 const Options = require('./Components/Options.jsx');
 
+
 reactDOM.render(<Options />, getElementById('Options'));
