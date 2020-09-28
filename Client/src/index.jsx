21963764bbb437c50b399213ab997538c085f150
David Boyea <davidmcruz1125@gmail.com>
2020-09-28 14:37:14 -0500

    Builds out an index.jsx file

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,1 +1,5 @@
-// this is will be the renderin with react will start for webpac
+const react = require('react');
+const reactDOM = require('react-dom');
+const Options = require('./Components/Options.jsx');
+
+reactDOM.render(<Options />, getElementById('Options'));
