a9d7cfafdec17d206ba1a364cca86c3cb4e641fc
David Boyea <davidmcruz1125@gmail.com>
2020-09-29 12:55:39 -0500

    Options is viable and working to render and take on components

--- Client/src/Components/Options.jsx
+++ Client/src/Components/Options.jsx
@@ -1,26 +1,44 @@
 const React = require('react');
+import QualityControl from './QualityControl.jsx';
 
+let sample = {
+  _id: 0,
+  name: `65" Class Q900TS QLED 8K UHD HDR Smart TV (2020)`,
+  modelNum: "QN65Q900TSFXZA",
+  averageReview: {
+    stars: 4.8,
+    votes:31
+  },
+  insPlan2Yr: 499.99,
+  insPlan4Yr: 799.99,
+  installments: 93.74,
+  buyNow: 4999.99,
+  savings: 1000
+};
+/***************************************/
 
 class Options extends React.Component {
   constructor() {
     super();
     this.state = {
+      view: 0,
+      products: [sample]
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick () {
-    console.log('handle CLick')
+    console.log('handle CLick');
   }
 
   render() {
+    const { view, products } = this.state;
     return (
       <div className='specifications'>
-        <h4>PRODUCT NAME</h4><a onClick={this.handleClick} style={{ cursor: 'pointer' }}>Wishlist</a>{// add a heart to the left of the word Wishlist
+        <h4>PRODUCT NAME</h4><a onClick={ this.handleClick } style={{ cursor: 'pointer' }}>Wishlist</a>{// add a heart to the left of the word Wishlist
   }
         <br/>
-
-        <div className='quality-control'><p className='product-code'>N0NS3NS3NUM83R</p> <div className='all-stars'></div> <a onClick={this.handleClick} style={ { cursor: 'pointer' } }>Share</a> </div>
+        <QualityControl handleClick={ this.handleClick } object={ products[ view ] }/>
         <div className='description'><p>Breif 40 word description</p></div>
         <div className='possibilities'>
           <p>Choice of Size</p>
