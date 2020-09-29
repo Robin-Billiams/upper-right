9e63ba02b6e8be70d42d344ef09bcbe35c5e7f55
David Boyea <davidmcruz1125@gmail.com>
2020-09-29 18:50:39 -0500

    Implement Focus, and style with wire fime in place

--- Client/src/Components/QualityControl.jsx
+++ Client/src/Components/QualityControl.jsx
@@ -7,8 +7,8 @@ const QualityControl = ( { handleClick, object } ) => {
   return(
     <div className='quality-control'>
       <p className='product-code'>{ modelNum }</p>
-      <span className='all-stars'> { stars }_STARS { `(${votes})` } </span>
-      <p className='btn-share' onClick={handleClick} style={ styl }>Share</p>
+      <span className='all-stars'> _STARS{ stars }{ `(${votes})` } </span>
+      <p className='btn-share' onClick={handleClick} style={ styl }>Share your product experience</p>
     </div>
 
   )
