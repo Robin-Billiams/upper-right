aee7e4bfa342279c088fb02fa304fd7b08f2ec8c
David Boyea <davidmcruz1125@gmail.com>
2020-09-29 12:13:31 -0500

    Options displays to the page ugly sample data, proof of concept

--- Client/src/Components/Options.jsx
+++ Client/src/Components/Options.jsx
@@ -1,19 +1,34 @@
 const React = require('react');
 
-console.log('test');
 
 class Options extends React.Component {
   constructor() {
     super();
     this.state = {
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick () {
+    console.log('handle CLick')
   }
 
   render() {
     return (
-      <div>
-        <h1>HELLO WORLD!!</h1>
+      <div className='specifications'>
+        <h4>PRODUCT NAME</h4><a onClick={this.handleClick} style={{ cursor: 'pointer' }}>Wishlist</a>{// add a heart to the left of the word Wishlist
+  }
+        <br/>
+
+        <div className='quality-control'><p className='product-code'>N0NS3NS3NUM83R</p> <div className='all-stars'></div> <a onClick={this.handleClick} style={ { cursor: 'pointer' } }>Share</a> </div>
+        <div className='description'><p>Breif 40 word description</p></div>
+        <div className='possibilities'>
+          <p>Choice of Size</p>
+          <br/>
+          <div className='selectors'></div>
+        </div>
+
+
       </div>
     )
   }
