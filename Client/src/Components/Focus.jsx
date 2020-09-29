d190ebefe2472b1bf80987760d79b5d10013ff6c
David Boyea <davidmcruz1125@gmail.com>
2020-09-29 15:07:09 -0500

    adjusted git to ignore package.json

---
+++ Client/src/Components/Focus.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Focus = (props) => {
+
+}
+
+export default Focus;
--- Client/src/Components/QualityControl.jsx
+++ Client/src/Components/QualityControl.jsx
@@ -6,7 +6,9 @@ const QualityControl = ( { handleClick, object } ) => {
   const styl = { cursor: 'pointer' };
   return(
     <div className='quality-control'>
-      <p className='product-code'>{ modelNum }</p><span className='all-stars'> { stars }_STARS{ votes } </span><p className='btn-share' onClick={handleClick} style={ styl }>Share</p>
+      <p className='product-code'>{ modelNum }</p>
+      <span className='all-stars'> { stars }_STARS { `(${votes})` } </span>
+      <p className='btn-share' onClick={handleClick} style={ styl }>Share</p>
     </div>
   )
 }
