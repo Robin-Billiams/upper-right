38e52d04522d608b31a7bde2ab85136c4a0f05a9
David Boyea <davidmcruz1125@gmail.com>
2020-09-30 19:49:59 -0500

    Binds index to the select function for the choice buttons

--- Client/src/Components/Choices.jsx
+++ Client/src/Components/Choices.jsx
@@ -4,17 +4,17 @@ const Choices = ( { select, selected, options, prices } ) => {
   return (
     <div className='Choices'>
       {options.map( ( choice, index ) => {
-        console.log(choice + selected)
+        console.log( index + selected )
         let difference = prices[ index ] - prices[ selected ] ;
         if ( index === selected ) {
           return (
-            <sapn onClick={ ( index ) => { select( index ) } } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
+            <sapn onClick={select.bind( this, index ) } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
               { choice }
             </sapn>
           )
         } else {
           return (
-            <sapn onClick={ ( index ) => { select( index ) } } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
+            <sapn onClick={ select.bind( this, index ) } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
               { choice } +${ difference }
             </sapn>
           )
