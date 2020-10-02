d3ee2944c0d40e0bfb239c5f42b33a6556d8d939
David Boyea <davidmcruz1125@gmail.com>
2020-10-02 17:46:04 -0500

    Adds icon and shipping label

--- Client/src/Components/Options.jsx
+++ Client/src/Components/Options.jsx
@@ -4,13 +4,15 @@ import Focus from './Focus.jsx';
 import Choices from './Choices.jsx';
 import Protection from './Protection.jsx';
 import Financing from './Financing.jsx';
+import SVG from './SVG/icons.jsx';
+import Moment from 'moment';
 
-let geoFlag = <svg className="svg-icon" viewBox="0 0 20 20"> <path d="M10,1.375c-3.17,0-5.75,2.548-5.75,5.682c0,6.685,5.259,11.276,5.483,11.469c0.152,0.132,0.382,0.132,0.534,0c0.224-0.193,5.481-4.784,5.483-11.469C15.75,3.923,13.171,1.375,10,1.375 M10,17.653c-1.064-1.024-4.929-5.127-4.929-10.596c0-2.68,2.212-4.861,4.929-4.861s4.929,2.181,4.929,4.861C14.927,12.518,11.063,16.627,10,17.653 M10,3.839c-1.815,0-3.286,1.47-3.286,3.286s1.47,3.286,3.286,3.286s3.286-1.47,3.286-3.286S11.815,3.839,10,3.839 M10,9.589c-1.359,0-2.464-1.105-2.464-2.464S8.641,4.661,10,4.661s2.464,1.105,2.464,2.464S11.359,9.589,10,9.589"></path></svg>
 
-let plus = <svg className="plus-icon" viewBox="0 0 20 20"> <path d="M14.613,10c0,0.23-0.188,0.419-0.419,0.419H10.42v3.774c0,0.23-0.189,0.42-0.42,0.42s-0.419-0.189-0.419-0.42v-3.774H5.806c-0.23,0-0.419-0.189-0.419-0.419s0.189-0.419,0.419-0.419h3.775V5.806c0-0.23,0.189-0.419,0.419-0.419s0.42,0.189,0.42,0.419v3.775h3.774C14.425,9.581,14.613,9.77,14.613,10 M17.969,10c0,4.401-3.567,7.969-7.969,7.969c-4.402,0-7.969-3.567-7.969-7.969c0-4.402,3.567-7.969,7.969-7.969C14.401,2.031,17.969,5.598,17.969,10 M17.13,10c0-3.932-3.198-7.13-7.13-7.13S2.87,6.068,2.87,10c0,3.933,3.198,7.13,7.13,7.13S17.13,13.933,17.13,10"></path></svg>
+let truck = SVG.truck;
+let geoFlag = SVG.geoFlag;
+let plus = SVG.plus;
+let tag = SVG.tag;
 
-let tag = <svg className="tag-icon" viewBox="0 0 20 20" fill='grey'> <path d="M17.35,2.219h-5.934c-0.115,0-0.225,0.045-0.307,0.128l-8.762,8.762c-0.171,0.168-0.171,0.443,0,0.611l5.933,5.934c0.167,0.171,0.443,0.169,0.612,0l8.762-8.763c0.083-0.083,0.128-0.192,0.128-0.307V2.651C17.781,2.414,17.587,2.219,17.35,2.219M16.916,8.405l-8.332,8.332l-5.321-5.321l8.333-8.332h5.32V8.405z M13.891,4.367c-0.957,0-1.729,0.772-1.729,1.729c0,0.957,0.771,1.729,1.729,1.729s1.729-0.772,1.729-1.729C15.619,5.14,14.848,4.367,13.891,4.367 M14.502,6.708c-0.326,0.326-0.896,0.326-1.223,0c-0.338-0.342-0.338-0.882,0-1.224c0.342-0.337,0.881-0.337,1.223,0C14.84,5.826,14.84,6.366,14.502,6.708"></path>
-</svg>
 let sample = {
   _id: 0,
   name: [`65" Class Q900TS QLED 8K UHD HDR Smart TV (2020)`, `75" Class Q900TS QLED 8K UHD HDR Smart TV (2020)`, `85" Class Q900TS QLED 8K UHD HDR Smart TV (2020)`],
@@ -96,16 +98,21 @@ class Options extends React.Component {
         <span className='buy-now-span' onClick={ () => { this.handleClick( 'BUY NOW' ) } } style={ { cursor: 'pointer' } }>
           <b><p className='buy-now'>B U Y &nbsp; &nbsp; N O W</p></b>
         </span>
-        <br/>
         <div className='store-finder' onClick={ () => { this.handleClick( ' store finder' ) } } style={ { cursor: 'pointer' } }>
           <span className='geo-flag'>{geoFlag}</span>
           <p className='where-to-buy'>Where to buy</p>
         </div>
+        <div className='e-t-a'>
+          {truck}<p className='free-shipping'><b>Free shipping and free 15-day returns</b></p>
+          <br/>
 
+        </div>
       </div>
     )
   }
 }
+
+
 // $.ajax({
 //   url: `/api/customizePurchase`,
 //   type: `GET`,
