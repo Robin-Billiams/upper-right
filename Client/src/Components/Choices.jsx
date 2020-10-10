40ab933002a8183ea1cf2d1557f6690da7fffee6
David Boyea <davidmcruz1125@gmail.com>
2020-10-09 21:50:17 -0500

    fix

--- Client/src/Components/Choices.jsx
+++ Client/src/Components/Choices.jsx
@@ -4,18 +4,18 @@ const Choices = ( { select, selected, options, prices } ) => {
   return (
     <div className='Choices'>
       {options.map( ( choice, index ) => {
-        let difference = index < selected ? `-$${ ( ( Math.abs( prices[ index ] - prices[ selected ] ) ).toFixed( 2 ) ).toLocaleString( 'en' ) }` : `+$${ ( ( prices[ index ] - prices[ selected ] ).toFixed( 2 ) ).toLocaleString( 'en' ) }`
+        let difference = index < selected ? `-$${ Number( ( Math.abs( prices[ index ] - prices[ selected ] ) ).toFixed( 2 ) ).toLocaleString( 'en' ) }` : `+$${ Number( ( prices[ index ] - prices[ selected ] ).toFixed( 2 ) ).toLocaleString( 'en' ) }`
         console.log( difference)
         if ( index === selected ) {
           return (
             <span onClick={select.bind( this, index ) } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
-              <b className='choice'>{ choice }</b>
+              <b className='choice'>The { choice }</b>
             </span>
           )
         } else {
           return (
             <span onClick={ select.bind( this, index ) } style={ { cursor: 'pointer' } } className='option-select' key={ index }>
-              <b className='choice'>{ choice }</b> { difference }
+              <b className='choice'>The { choice }</b> { difference }
             </span>
           )
         }
