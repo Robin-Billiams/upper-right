e40101503a09d337080cbbc4a720d5df63b64da2
David Boyea <davidmcruz1125@gmail.com>
2020-10-10 19:10:45 -0500

    Server port change

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/../Client/dist'));
 
-const port = 3002;
+const port = 4003;
 
 // app.get('/', ( req, res ) => {
 //   res.sendFile(`index.html`, {root: './Client/dist'});
